1745c35c431648ea5a34952dcf08797147490e10
Santiago <santiagomorales333@yahoo.com>
2025-03-30 10:10:22 -0700

    handling errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -28,9 +28,13 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
 
-  await sql`
+  try {
+    await sql`
   INSERT INTO invoices (customer_id, amount, status, date)
   VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
+  } catch (error) {
+    console.log(error);
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
@@ -45,17 +49,21 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100;
 
-  await sql`
+  try {
+    await sql`
   UPDATE invoices
   SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
   WHERE id = ${id}`;
 
-  revalidatePath("/dashboard/invoices");
-  redirect("/dashboard/invoices");
+    revalidatePath("/dashboard/invoices");
+    redirect("/dashboard/invoices");
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export async function deleteInvoice(id: string) {
   await sql`
-    DELETE FROM invoices WHERE id = ${id}`;
+        DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath("/dashboard/invoices");
 }
